8719bb72d63a0fee1e3abb686dfdd5ebf1337c6f
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-11-16 10:23:28 -0800

    fix mirror issue that caused test 1 to always pass and now boht tests pass correctly

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -6,12 +6,12 @@ class Test
 
   def anagram?
   array1 = @word1.split('')
-  array2 = @word1.split('')
+  array2 = @word2.split('')
 
   if array1 == array2
     return "these are the same word"
-  # elsif array1 != array2
-  #   return "these are not the same word"
+  elsif array1 != array2
+    return "these are not the same word"
   end
   end
 end
