13c7c33c2c800de483c2792f846c9f501de77e4c
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-11-16 11:24:08 -0800

    create test that makes sure all words are 'real'

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -7,14 +7,18 @@ describe('#anagram?') do
     test1 = Test.new("cat", "cat")
     expect(test1.anagram?()).to(eq('these are the same word'))
   end
+  it("it checks to see if two words are anagrams") do
+    test1 = Test.new("race", "care")
+    expect(test1.anagram?()).to(eq('these words are anagrams'))
+  end
+  it("it checks to see if two words are actually 'words'(contain a vowel)") do
+    test1 = Test.new("hjl", "ljh")
+    expect(test1.anagram?()).to(eq('please enter a real word'))
+  end
 
   # it("it checks to see if two words are different") do
   #   test1 = Test.new("cat", "dog")
   #   expect(test1.anagram?()).to(eq('these are not the same word'))
   # end
 
-  it("it checks to see if two words are anagrams") do
-    test1 = Test.new("race", "care")
-    expect(test1.anagram?()).to(eq('these words are anagrams'))
-  end
 end
