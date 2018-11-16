0feba997dc3aa93c6f0b8667fe44b6781da065f1
MrDanielBennett <daniel.bennett1999@gmail.com>
2018-11-16 10:17:56 -0800

    test 1 now passes

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -4,12 +4,14 @@ class Test
     @word2 = word2
   end
 
-  def anagram?()
+  def anagram?
   array1 = @word1.split('')
   array2 = @word1.split('')
 
   if array1 == array2
     return "these are the same word"
+  elsif array1 != array2
+    return "these are not the same word"
   end
   end
 end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -2,9 +2,9 @@ require('rspec')
 require('anagrams_antigrams')
 require('pry')
 
-describe('#anagram') do
-  it("it checks to see if two words are the same")do
+describe('#anagram?') do
+  it("it checks to see if two words are the same") do
     test1 = Test.new("cat", "cat")
-    expect(anagram?()).to(eq('these are the same word'))
+    expect(test1.anagram?()).to(eq('these are the same word'))
   end
 end
